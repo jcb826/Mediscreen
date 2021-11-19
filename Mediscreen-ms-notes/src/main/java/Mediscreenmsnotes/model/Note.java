4140895a490930019d8c18eca24cb274aacb2434
jc <SM@shed31>
2021-11-19 14:17:11 +0100

    implementing noteDate

--- Mediscreen-ms-notes/src/main/java/Mediscreenmsnotes/model/Note.java
+++ Mediscreen-ms-notes/src/main/java/Mediscreenmsnotes/model/Note.java
@@ -4,20 +4,24 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+
 @Document("note")
 public class Note {
     @Id
     public String id;
     public Long patientId;
     public String note;
+    private LocalDate noteDate;
 
     public Note() {
     }
 
-    public Note(String id, Long patientId, String note) {
+    public Note(String id, Long patientId, String note, LocalDate noteDate) {
         this.id = id;
         this.patientId = patientId;
         this.note = note;
+        this.noteDate = noteDate;
     }
 
     public String getId() {
@@ -42,6 +46,14 @@ public class Note {
 
     public void setNote(String note) {
         this.note = note;
+    }
+
+    public LocalDate getNoteDate() {
+        return noteDate;
+    }
+
+    public void setNoteDate(LocalDate noteDate) {
+        this.noteDate = noteDate;
     }
 
     @Override
--- Mediscreen-webApp/src/main/java/MediscreenwebApp/model/Note.java
+++ Mediscreen-webApp/src/main/java/MediscreenwebApp/model/Note.java
@@ -5,7 +5,61 @@ import java.time.LocalDate;
 
 public class Note {
 
-    public String id;
-    public Long patientId;
-    public String note;
+    private String id;
+    private Long patientId;
+    private String note;
+    private LocalDate noteDate;
+
+
+    public Note() {
+    }
+
+    public Note(String id, Long patientId, String note, LocalDate noteDate) {
+        this.id = id;
+        this.patientId = patientId;
+        this.note = note;
+        this.noteDate = noteDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public LocalDate getNoteDate() {
+        return noteDate;
+    }
+
+    public void setNoteDate(LocalDate noteDate) {
+        this.noteDate = noteDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id='" + id + '\'' +
+                ", patientId=" + patientId +
+                ", note='" + note + '\'' +
+                ", noteDate=" + noteDate +
+                '}';
+    }
 }
