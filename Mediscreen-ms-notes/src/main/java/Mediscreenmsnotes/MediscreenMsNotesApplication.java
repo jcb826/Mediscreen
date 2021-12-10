d5822c002fae95c77d6a5947a9a70667d1c99602
jc <SM@shed31>
2021-12-10 12:07:31 +0100

    ending jacoco

--- Mediscreen-ms-notes/src/main/java/Mediscreenmsnotes/MediscreenMsNotesApplication.java
+++ Mediscreen-ms-notes/src/main/java/Mediscreenmsnotes/MediscreenMsNotesApplication.java
@@ -2,8 +2,10 @@ package Mediscreenmsnotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class MediscreenMsNotesApplication {
 
 	public static void main(String[] args) {
